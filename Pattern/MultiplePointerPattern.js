399006e889a22b2c267546bdd2be2491881103bd
AnudeepN <n.anudeep@ericsson.com>
2019-03-21 14:51:27 +0530

    new data

--- Pattern/MultiplePointerPattern.js
+++ Pattern/MultiplePointerPattern.js
@@ -72,3 +72,72 @@ function countUniqueValues(arr) {
 }
 countUniqueValues([1, 1, 2, 2, 5, 7, 7, 99]);
 //uses single loop to check the unique values and it works with O(n) time complexity.
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+ 
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
+}
+
+findLongestSubstring("ritthmschool")
+
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+ 
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then 
+		// move the window to right
+    if(total < sum && end < nums.length){
+      total += nums[end];
+			end++;
+    }
+    // if current window adds up to at least the sum given then
+		// we can shrink the window 
+    else if(total >= sum){
+      minLen = Math.min(minLen, end-start);
+			total -= nums[start];
+			start++;
+    } 
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    else {
+      break;
+    }
+  }
+ 
+  return minLen === Infinity ? 0 : minLen;
+}
+
+minSubArrayLen([2,3,1,2,4,3],7);
+
+function maxSubArraySum(arr, num) {
+  let tempVal = 0,
+    maxVal = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxVal += arr[i];
+  }
+  tempVal = maxVal;
+
+  for (let i = num; i < arr.length; i++) {
+    tempVal = tempVal - arr[i - num] + arr[i];
+    maxVal = Math.max(maxVal, tempVal);
+  }
+
+  return maxVal;
+}
+
+ maxSubarraySum([100,200,300,400],2);
